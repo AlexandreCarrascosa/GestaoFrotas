fab7d8dd7ef84124a31c7bcf497c20b305b712e7
AlexandreCarrascosa <alexandrecarrascosa@gmail.com>
2025-05-17 16:16:33 -0400

    feat: update project

--- app/main.py
+++ app/main.py
@@ -1,15 +1,30 @@
-from fastapi import FastAPI
-from app.routes import reserva
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from app.routes import reserva
 
 app = FastAPI()
 
-# CORS para permitir chamadas do frontend
+# CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # ou coloque o domínio específico
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Rota da API
 app.include_router(reserva.router, prefix="/api")
+
+# Static files (CSS/JS)
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+# Templates (HTML)
+templates = Jinja2Templates(directory="frontend")
+
+# Página principal
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
